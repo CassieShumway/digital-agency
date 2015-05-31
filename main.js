d397260bb7c435518e84333819adb216c94fdd2c
cassie shumway <cassieshumway@gmail.com>
2015-05-31 00:19:28 -0400

    try different video embed

--- main.js
+++ main.js
@@ -18,26 +18,6 @@ $( document ).ready(function() {
 	      scrollTop: $("#news").offset().top - $headerHeight
 	  }, 1000);
 	});
-
-	//Youtube video aspect ratio
-	var $allVideos = $("iframe[src^='//www.youtube.com']"),
-	    $fluidEl = $("body");
-		$allVideos.each(function() {
-		  $(this)
-		    .data('aspectRatio', this.height / this.width)
-		    .removeAttr('height')
-		    .removeAttr('width');
-		});
-
-	$(window).resize(function() {
-	  var newWidth = $fluidEl.width();
-	  $allVideos.each(function() {
-	    var $el = $(this);
-	    $el
-	      .width(newWidth)
-	      .height(newWidth * $el.data('aspectRatio'));
-	  });
-	}).resize();
 });
 
 
